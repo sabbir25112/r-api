aa144a92b1f095f03b9e54d4e9025f0c86514d1f
sabbir25112 <sabbir25112@gmail.com>
2022-08-20 21:25:51 +0600

    added order, parcelOrder, Parcel api

--- app/Order/Status.php
+++ app/Order/Status.php
@@ -5,4 +5,11 @@ namespace App\Order;
 class Status
 {
     const PLACED = 'placed';
+    const ACCEPTED = 'accepted';
+    const PICKED_UP = 'picked_up';
+    const DELIVERING = 'delivering';
+    const ON_COURIER = 'on_courier';
+    const CANCELLED = 'cancelled';
+    const DELIVERED = 'delivered';
+    const COMPLETED = 'completed';
 }
