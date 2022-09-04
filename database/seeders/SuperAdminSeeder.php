af5499b1ad17aed09984aaa80fb0a43bd19fe5ee
Sakib Ahmed <sakibrisa@gmail.com>
2022-09-04 15:34:49 +0600

    admin role and admin permission added on seeder

--- database/seeders/SuperAdminSeeder.php
+++ database/seeders/SuperAdminSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\Role;
 use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class SuperAdminSeeder extends Seeder
 {
@@ -14,13 +16,41 @@ class SuperAdminSeeder extends Seeder
      */
     public function run()
     {
+        // ADD SUPER ADMIN
         User::insert([
             [
-                'name' => 'Admin',
-                'email' => 'admin@rapido.com',
-                'password' => bcrypt(12345678),
+                'name'      => 'Admin',
+                'email'     => 'admin@rapido.com',
+                'password'  => bcrypt(12345678),
                 'user_type' => User::SUPERADMIN,
             ]
+        ]);
+
+        // ADD ADMIN ROLE
+        Role::insert([
+            [
+                'name'          => 'Admin',
+                'guard_name'    => 'api',
+            ]
+        ]);
+
+        // ASSIGN ALL PERMISSIONS TO ADMIN ROLE
+        for($i = 1; $i<=42; $i++){
+            DB::table('role_has_permissions')->insert([
+                [
+                    'permission_id' => $i,
+                    'role_id'       => 1,
+                ],
+            ]);
+        }
+
+        //ASSIGN ADMIN ROLE TO SUPER ADMIN
+        DB::table('model_has_roles')->insert([
+            [
+                'role_id'       => 1,
+                'model_type'    => 'App\Models\User',
+                'model_id'      => 1,
+            ],
         ]);
     }
 }
